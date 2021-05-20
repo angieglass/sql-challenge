d956e22f78ace3e9c548723ec7a9c0f8afef63c1
angiecruz <angiecruzalmanza@gmail.com>
2021-05-19 19:57:02 -0500

    Table schema done, starting queries

---
+++ queries.sql
@@ -0,0 +1,8 @@
+--Checking the tables loaded with the CSV files 
+
+SELECT * FROM departments; 
+SELECT * FROM dept_emp; 
+SELECT * FROM dept_manager; 
+SELECT * FROM employees; 
+SELECT * FROM salaries; 
+SELECT * FROM titles; 
